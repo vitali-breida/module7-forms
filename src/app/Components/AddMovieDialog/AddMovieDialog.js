2c8fb4f4b0d80534afe914115b6c08f6b9ab513a
vitali-breida <vitaly2511@tut.by>
2021-04-03 23:35:43 +0300

    Added full list of genres

--- src/app/Components/AddMovieDialog/AddMovieDialog.js
+++ src/app/Components/AddMovieDialog/AddMovieDialog.js
@@ -126,11 +126,20 @@ export default function AddMovieDialog(props) {
                 onChange={formik.handleChange}
                 error={formik.touched.genres && Boolean(formik.errors.genres)}
               >
+                <MenuItem value="Action">Action</MenuItem>
+                <MenuItem value="Adventure">Adventure</MenuItem>
+                <MenuItem value="Animation">Animation</MenuItem>
                 <MenuItem value="Drama">Drama</MenuItem>
+                <MenuItem value="Comedy">Comedy</MenuItem>
                 <MenuItem value="Crime">Crime</MenuItem>
                 <MenuItem value="Documentary">Documentary</MenuItem>
+                <MenuItem value="Fantasy">Fantasy</MenuItem>
+                <MenuItem value="Family">Family</MenuItem>
                 <MenuItem value="Horror">Horror</MenuItem>
-                <MenuItem value="Comedy">Comedy</MenuItem>
+                <MenuItem value="Romance">Romance</MenuItem>
+                <MenuItem value="Science Fiction">Science Fiction</MenuItem>
+                <MenuItem value="Thriller">Thriller</MenuItem>
+                <MenuItem value="War">War</MenuItem>
               </Select>
               <FormHelperText
                 error={formik.touched.genres && Boolean(formik.errors.genres)}
